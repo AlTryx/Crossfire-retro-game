36f33b61b38d56eada8870b2ea675860fb1fe69f
Aleksandar Stanchev <altryx15@gmail.com>
2024-05-19 14:04:11 +0300

    I've made the math to draw the corridors and remove their borders.
    Now I've got to apply that to all the corridors in the grid dynamically

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
     const grid = document.querySelector('.grid')
     const resultDisplay = document.querySelector('.result')
-
+    const width = 30;
+    const corridor = document.querySelector('.corridor')
+    
     //draw the grid
     for(let i = 0; i < 195; i++) {
         const square = document.createElement('div')
@@ -8,3 +10,14 @@
     }
 
     const squares = Array.from(document.querySelectorAll('.grid div'))
+    
+
+    // draw the corridors
+    function drawCorridors(){
+        for(let i = 0; i<squares.length;i++) {
+            if(i%2 !== 0 && i<14) {
+                squares[i].classList.add('corridor')
+            }
+        }
+    }
+    drawCorridors()
