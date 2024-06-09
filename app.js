65c88dd95d243c69542b6da2e338827730fedbb0
Aleksandar Stanchev <altryx15@gmail.com>
2024-06-09 20:20:57 +0300

    I did the base movement of the player

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     const resultDisplay = document.querySelector('.result')
     const corridor = document.querySelector('.corridor')
     const laserIndex = document.querySelector('.laser')
-    const width = 30
+    const width = 15
     let player
     let currentPlayerIndex
 
@@ -45,9 +45,43 @@
 
     //move the player
 
-    function movePlayer() {
+    function movePlayer(e) {
+        switch(e.key){
+            case 'ArrowLeft':
+                if(currentPlayerIndex % width !==0) {
+                    squares[currentPlayerIndex].classList.remove('player')
+                    squares[currentPlayerIndex -= 1]
+                    squares[currentPlayerIndex].classList.add('player')
+                }
+                break
 
+            case 'ArrowRight':
+                if(currentPlayerIndex % width < width - 1) {
+                    squares[currentPlayerIndex].classList.remove('player')
+                    squares[currentPlayerIndex += 1]
+                    squares[currentPlayerIndex].classList.add('player')
+                }
+                break
+
+            case 'ArrowUp':
+                if(currentPlayerIndex - width >=0) {
+                    squares[currentPlayerIndex].classList.remove('player')
+                    squares[currentPlayerIndex -= width]
+                    squares[currentPlayerIndex].classList.add('player')
+                }
+                break
+
+            case 'ArrowDown':
+                if (currentPlayerIndex + width < width*width) {
+                    squares[currentPlayerIndex].classList.remove('player')
+                    squares[currentPlayerIndex += width]
+                    squares[currentPlayerIndex].classList.add('player')
+                }
+                break
+        }
     }
+
+    document.addEventListener('keyup', movePlayer)
 
     //drawing the enemy
     
@@ -62,3 +96,9 @@
     }
 
     setInterval(drawLaser, 2000)
+
+    // I got came up with an idea that the player can "hide" in the walls for a short amount of time so they can hide from enemies. If they last longer than the timer for hiding, the game is over.
+    // after the player escape the hiding spot, the hiding area gets the class of 'used-hiding-spot' and it turns RED! which means you can't hide there anymore. There's a cooldown before you can hide anywhere again.
+    // the enemies will grow stronger as the time passes and the score builds higher.
+    //game win - a certain amount of score or until timer ends
+    //game loss - if being killed by an enemy.
